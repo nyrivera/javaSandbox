8070aa43502e63ef2d9ae01a344cb62233b3b88b
nyrivera <68795543+nyrivera@users.noreply.github.com>
2021-04-09 05:57:19 -0700

    Add files via upload

---
+++ src/javaSandbox/Troll.java
@@ -0,0 +1,18 @@
+package javaSandbox;
+
+public class Troll {
+
+	public static void main(String[] args) {
+		String str= "This website is for losers LOL!";
+	}
+	public static String disemvowel(String str) {
+		
+		for(int i=0; i<= str.length(); i++) {
+			System.out.println("test");
+		}
+		
+		
+		return " ";
+	}
+
+}
